d9c00ad3f949c96b589ca654d5e7c854b557c247
ashikur-rahman-fh <ashikurrahmanvar@gmail.com>
2022-10-08 07:50:28 -0400

    Add a sample json route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/sample', (req, res, next) => {
+  res.json({ foo: "bar" });
+});
+
 module.exports = router;
