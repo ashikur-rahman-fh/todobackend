3101fe78529dfc6525271d2d0b61d31554de91b4
ashikur-rahman-fh <ashikurrahmanvar@gmail.com>
2022-10-16 07:54:04 -0400

    Add fix to the production db name error.

--- services/db.js
+++ services/db.js
@@ -3,9 +3,10 @@ const { DATABASE_CONFIG } = require('../constants/e_variables');
 
 const connectDatabase = async () => {
     const uri = `mongodb+srv://tododb:${DATABASE_CONFIG.PASSWORD}@todo.wj0akci.mongodb.net/?retryWrites=true&w=majority`
+    console.log(DATABASE_CONFIG.NAME);
     const options = {
         ssl: DATABASE_CONFIG.MODE === 'dev' || DATABASE_CONFIG.MODE === 'prod',
-        dbName: DATABASE_CONFIG.DB_NAME,
+        dbName: DATABASE_CONFIG.NAME,
     };
     await mongoose.connect(uri, { ...options });
 }
