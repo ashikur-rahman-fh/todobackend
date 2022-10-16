86b201c99890078c3cfac99adc219df3055feda9
ashikur-rahman-fh <ashikurrahmanvar@gmail.com>
2022-10-16 07:56:59 -0400

    Add default db if the db is not prodives

--- services/db.js
+++ services/db.js
@@ -3,10 +3,9 @@ const { DATABASE_CONFIG } = require('../constants/e_variables');
 
 const connectDatabase = async () => {
     const uri = `mongodb+srv://tododb:${DATABASE_CONFIG.PASSWORD}@todo.wj0akci.mongodb.net/?retryWrites=true&w=majority`
-    console.log(DATABASE_CONFIG.NAME);
     const options = {
         ssl: DATABASE_CONFIG.MODE === 'dev' || DATABASE_CONFIG.MODE === 'prod',
-        dbName: DATABASE_CONFIG.NAME,
+        dbName: DATABASE_CONFIG.NAME || 'wrongdb',
     };
     await mongoose.connect(uri, { ...options });
 }
